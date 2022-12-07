c1539bbb7a4ebc9fa4c79d1ef26108b2332d9438
kimyw2357 <bubuchachakk@gmail.com>
2022-12-07 17:31:32 +0900

    API CRUD

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 
-const tweets = [
+let tweets = [
   {
     id: '1',
     text: 'お金が欲しい！！',
@@ -24,9 +24,49 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
   const username = req.query.username;
   const data = username
-    ? tweets.filter(t => t.username === username)
+    ? tweets.filter(tweet => tweet.username === username)
     : tweets;
     res.status(200).json(data);
 });
 
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const tweet = tweets.find(tweet => tweet.id === id);
+  if (tweet) {
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({messag: `tweet id(${id}) not found`});
+  }
+});
+
+router.post('/', (req, res, next) => {
+  const {text, name, username} = req.body;
+  const tweet = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date(),
+    name,
+    username,
+  }
+  tweets = [tweet, ...tweets];
+  res.status(201).json(tweet);
+});
+
+router.put('/:id', (req, res, next) => {
+  const id = req.params.id;
+  const text = req.body.text;
+  const tweet = tweets.find(tweet => tweet.id === id);
+  if(tweet) {
+    tweet.text = text;
+    res.status(200).json(tweet);
+  } else {
+    res.status(404).json({messag: `tweet id(${id}) not found`});
+  }
+
+})
+router.delete('/:id', (req, res, next) => {
+  const id = req.params.id
+  tweets = tweets.filter(tweet => tweet.id !== id);
+  res.sendStatus(204);
+})
 export default router;
